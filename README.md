ba7969c03c0b5799ae6c5bb3fb8befa0089b4674
Stefan Michel <info@stefan-michel.com>
2022-06-20 10:01:34 +0200

    smichel - project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# URL Shortener
+This is a basic URL Shortener build in Java, Spring Boot and H2.
+
+## How to use
---
+++ src/test/java/com/stefanmichel/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanmichel.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
